c092b961ff14f4f84e4f9d3098b2821d4a40a514
Robin <booksofcodeandpsychic@gmail.com>
2025-05-06 15:48:38 -0400

    debug

--- lexer.cpp
+++ lexer.cpp
@@ -7,6 +7,7 @@
 
 int lineNumber = 1;
 char ch;
+std::string tokenType = "Type";
 
 struct Token {
 	std::string type;
@@ -14,14 +15,15 @@ struct Token {
 	int line;
 };
 
-std::vector<Token> tokenize(std::vector<char>& buf, std::ifstream& src) {
-	std::vector<Token> tokens;
+std::vector<Token> tokenize(std::vector<Token>& tokens, std::vector<char>& buf, std::ifstream& src) {
 	std::string value(buf.begin(), buf.end());
-	tokens.push_back({"Type", value, lineNumber});
+	tokens.push_back({tokenType, value, lineNumber});
+	buf.clear();
 	return tokens;
 }
 
 int main(int argc, char *argv[]) {
+	std::vector<Token> tokens;
 	std::ifstream src;
 	std::string srcPath = argv[1];
 	src.open(srcPath);
@@ -37,17 +39,59 @@ int main(int argc, char *argv[]) {
 			if (ch == '\n') {
 				lineNumber++;
 			}
-		for (const Token& token : tokenize(buf, src)) {
-			std::cout << "\nType: " << token.type << std::endl;
-			std::cout << "Value: " << token.value;
-			std::cout <<"\nLine Number: " << token.line;
-		}
-			buf.clear();
+
+			if (tokenType != "String") {
+				tokenize(tokens, buf, src);
+			}
+			
+			else {
+				buf.push_back(ch);
+			}
 		}
 
-		else {
-			buf.push_back(ch);
+		else if (ch == '\"') {
+			if (tokenType != "String") {
+				tokenType = "String";
+				buf.push_back(ch);
+				tokenize(tokens, buf, src);
+			}
+
+			else {
+				buf.pop_back();
+				tokenize(tokens, buf, src);
+				tokenType = "Type";
+			}
 		}
+		
+		else if (tokenType != "String") {
+			if (ch == '(') {
+				tokenType = "(";
+				buf.push_back(ch);
+				tokenize(tokens, buf, src);
+			}
+
+			else if (ch == ')') {
+				tokenType = ")";
+				buf.push_back(ch);
+				tokenize(tokens, buf, src);
+			}
+			
+			else if (ch == '{') {
+				tokenType = "{";
+				buf.push_back(ch);
+				tokenize(tokens, buf, src);
+			}
+
+			else {
+				buf.push_back(ch);
+			}
+		}
+	}
+
+	for (const Token& token : tokens) {
+		std::cout << "\n\nType: " << token.type;
+		std::cout << "\nValue: " << token.value;
+		std::cout << "\nLine: " << token.line;
 	}
 
 	return 0;
