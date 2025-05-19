9a19cbab5c87bc9adbdae0dff6573617ab024555
Robin <booksofcodeandpsychic@gmail.com>
2025-05-19 20:59:59 +0000

    Removed irrelevant includes

--- bash.sh
+++ bash.sh
@@ -1,3 +1,3 @@
 reset
-g++ -std=gnu++2b -g -I/home/robin/lexy/include -o falcon src/lexer.cpp src/parser.cpp src/compiler.cpp
+g++ -std=gnu++2b -g -o falcon src/lexer.cpp src/parser.cpp src/compiler.cpp
 sudo ./falcon examples/main.flcn
