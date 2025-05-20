74ef26d61a20dc315b25090d0ea2e0ddcf28cb39
Robin Wells <robinwells@Robins-Air.lan>
2025-05-19 20:58:03 -0400

    Cleaned repo

--- run_linux.sh
+++
@@ -1,2 +0,0 @@
-nasm -f elf64 asm/example.asm -o asm/example.o
-ld asm/example.o -o example
--- run_macos.sh
+++ run_macos.sh
@@ -1,3 +1,3 @@
-nasm -f macho64 asm/example.asm -o asm/example.o
-ld asm/example.o -o example -e _start -macosx_version_min 10.13 -static
-./example
+nasm -f macho64 asm/main.asm -o asm/main.o
+ld asm/main.o -o main -e _start -macosx_version_min 10.13 -static
+./main
